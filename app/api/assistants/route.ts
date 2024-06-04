6679ced3111a4d904c79f59b5f9f3eda64733835
Keito654 <keito19981129@gmail.com>
2024-06-04 22:00:48 +0900

    fixed

--- app/api/assistants/route.ts
+++ app/api/assistants/route.ts
@@ -1,38 +1,39 @@
 import { openai } from "@/app/openai";
 
-export const runtime = 'nodejs';
+export const runtime = "nodejs";
 
 // Create a new assistant
 export async function POST() {
   const assistant = await openai.beta.assistants.create({
-    instructions: "You are a helpful assistant.",
+    instructions:
+      "I'm an amateur at generative AI and can't give proper commands. In response to crude commands, please analogize and verbalize prompts that reflect the original intention and execute them. Also, please think in English and display your answers in Japanese. Please feel free to ask questions if you have any questions so that we can give you a better answer. If a question is asked in Japanese, please be sure to answer in Japanese.",
     name: "Quickstart Assistant",
     model: "gpt-4o",
     tools: [
-      { type: 'code_interpreter' },
+      { type: "code_interpreter" },
       {
-        type: 'function',
+        type: "function",
         function: {
-          name: 'get_weather',
-          description: 'Determine weather in my location',
+          name: "get_weather",
+          description: "Determine weather in my location",
           parameters: {
-            type: 'object',
+            type: "object",
             properties: {
               location: {
-                type: 'string',
-                description: 'The city and state e.g. San Francisco, CA'
+                type: "string",
+                description: "The city and state e.g. San Francisco, CA",
               },
               unit: {
-                type: 'string',
-                enum: ['c', 'f']
-              }
+                type: "string",
+                enum: ["c", "f"],
+              },
             },
-            required: ['location']
-          }
-        }
+            required: ["location"],
+          },
+        },
       },
-      { type: 'file_search' }
-    ]
+      { type: "file_search" },
+    ],
   });
   return Response.json({ assistantId: assistant.id });
 }
